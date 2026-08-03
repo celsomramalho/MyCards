a59f05357461d4d866c78e4dd03615344540edac
celsomramalho <celsomramalho@gmail.com>
2026-08-03 19:10:52 -0300

    MyCards_1.1.1b

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sobe-desce-v3";
+const CACHE = "sobe-desce-v4";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
